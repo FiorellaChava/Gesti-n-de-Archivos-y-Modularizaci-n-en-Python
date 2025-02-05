76325a3da0fddbf2b2e42171e5190e0e36929b8e
FiorellaChava <fiochava01@gmail.com>
2025-02-04 20:23:37 -0600

    actualizacion

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-# Proyecto: [Nombre del Proyecto]
-# Estudiante: [Nombre del Estudiante]
-# Fecha de Inicio: [dd/mm/aaaa]
-# Fecha de Entrega: [dd/mm/aaaa]
+# Proyecto: fiorella proyecto
+# Estudiante: fiorella chavarria
+# Fecha de Inicio: 2/4/2025
+# Fecha de Entrega: 2/4/2025
 # Descripción: Este archivo contiene el punto de entrada principal del proyecto.
 # Recuerda incluir tu nombre completo, la fecha en la que iniciaste el proyecto y la fecha estimada de entrega.
 # Esto ayuda a mantener un registro claro del trabajo realizado.
+
+from analisis_datos.estadisticas import media,calcular_mediana
+
+lista_parametro = [5,3,1,2,40]
+
+print('media: ', media(lista_parametro))
